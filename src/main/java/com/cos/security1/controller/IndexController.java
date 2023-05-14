39cc2b5251b9efdd6219da184448ae4bf5cf8ad0
ondjj <jcjk0302@gmail.com>
2023-05-15 08:37:11 +0900

    시큐리티 권한 처리
    SecurityConfig - @EnableGlobalMethodSecurity 속성 값에 따라 권한 처리를 부여할 수 있다.

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.cos.security1.controller;
 import com.cos.security1.model.User;
 import com.cos.security1.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -58,5 +60,17 @@ public class IndexController {
         user.setPassword(encPass);
         userRepository.save(user); // 회원가입은 잘되나 시큐리티로 로그인 불가 -> 패스워드가 암호화가 안되었기 때문에 !
         return "redirect:/loginForm";
+    }
+
+    @Secured("ROLE_ADMIN")
+    @GetMapping("/info")
+    public @ResponseBody String info(){
+        return "개인정보";
+    }
+
+    @PreAuthorize("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+    @GetMapping("/data")
+    public @ResponseBody String data(){
+        return "데이터정보";
     }
 }
