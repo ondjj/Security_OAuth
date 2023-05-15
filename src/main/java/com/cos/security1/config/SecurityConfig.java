cc76ab68b385c11393bdc39d96cab8e6ba86e143
ondjj <jcjk0302@gmail.com>
2023-05-15 10:19:16 +0900

    구글 로그인 준비
    - 구글은 카카오, 네이버와 달리 라이브러리에 메타 정보가 들어있다.
    
    - 그렇기 때문에 spring-boot-starter-oauth2-client 라이브러리를 추가한다면 별다른 설정이 필요 없지만 정해진 규칙대로 리다이렉트 URI를 지정해야한다.
    
    - 추가로 구글 로그인 버튼의 a 태그 href 속성 역시 지정되어있다.
    
    - SecurityConfig 에도 추가 설정이 필요하다.

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -37,7 +37,10 @@ public class SecurityConfig {
                 .formLogin()
                 .loginPage("/loginForm")
                 .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
-                .defaultSuccessUrl("/");
+                .defaultSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리 필요함
 
         return http.build();
     }
